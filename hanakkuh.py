8109d336d0919cdc21888d35fb91c1ad46ef8a6c
ct <194519830+CoThoma4430@users.noreply.github.com>
2025-06-17 01:33:03 -0400

    hanakkuh update

--- hanakkuh.py
+++ hanakkuh.py
@@ -35,13 +35,12 @@ con = duckdb.connect(database=':memory:')
 
 customers_path = './5784/noahs-customers.csv'
 orders_path = './5784/noahs-orders.csv'
-orders_items_path = './5784/noahs-orders_items.csv'
+items_path = './5784/noahs-orders_items.csv'
 products_path = './5784/noahs-products.csv'
 
 ### part 1
 
 customers_df = pd.read_csv('./5784/noahs-customers.csv')
-
 # customers_df.info()
 # print(customers_df['phone'])
 # print(customers_df.head())
@@ -52,11 +51,16 @@ pn_stripped = customers_df['phone'].str.replace('-', '')
 mask = t9_names == pn_stripped
 # print(customers_df[mask]['phone'])
 
+orders_df = pd.read_csv('./5784/noahs-orders.csv')
+# orders_df.info()
+# print(orders_df)
 
 ### part 2
 
-query = f"""
+### customers and orders query
+query_ordered = f"""
 select
+    o.orderid,
     c.customerid,
     c.name,
     o.ordered
@@ -68,24 +72,81 @@ on
  c.customerid = o.customerid
 """
 
-orders_items_df = pd.read_csv('./5784/noahs-orders_items.csv')
+### items dataframe creation
+items_df = pd.read_csv('./5784/noahs-orders_items.csv')
+# items_df.info()
+# print(items_df)
 
+### products dataframe creation
 products_df = pd.read_csv('./5784/noahs-products.csv')
+# products_df.info()
+# print(products_df['desc'])
+# print(products_df['sku'])
 
-# orders_items_df.info()
-# print(orders_items_df['sku'])
+### items and products query
+query_products = f"""
+select
+    o.orderid,
+    o.sku,
+    p.desc
+from
+    read_csv_auto ("{items_path}") o
+left join
+    read_csv_auto ("{products_path}") p
+on
+ o.sku = p.sku
+"""
 
-products_df.info()
-print(products_df['desc'])
+### combination of query_ordered and query_products
+query_combined = f"""
+select
+    o.orderid,
+    c.customerid,
+    c.name,
+    c.phone,
+    o.ordered,
+    i.sku,
+    p.desc
+from
+    read_csv_auto ("{customers_path}") c
+left join
+    read_csv_auto ("{orders_path}") o
+    on c.customerid = o.customerid
+left join
+    read_csv_auto ("{items_path}") i
+    on o.orderid = i.orderid
+left join
+    read_csv_auto ("{products_path}") p
+    on i.sku = p.sku
+where
+    extract(year from o.ordered::timestamp) = 2017
+    and regexp_replace(c.name, '([A-Z])[a-z]* ?', '\\1', 'g') = 'JP'
+    and lower(p.desc) like '%coffee%'
+"""
+    
 
-df = con.execute(query).fetchdf()
+### mask to find coffee sku inside products dataframe
+# mask = products_df['desc'].str.contains('coffee', case=False, na=False)
+# filtered_df = products_df[mask]
+# print(products_df[mask])
+
+### mask to find all the orderid's that got coffee
+df_prodorder = con.execute(query_products).fetchdf()
+mask_prodorder = df_prodorder['desc'].str.contains('coffee', case=False, na=False)
+# print(df_prodorder[mask_prodorder])
+
+### mask to find all of the JP's who ordered in 2017
+df = con.execute(query_ordered).fetchdf()
 var = pd.to_datetime(df['ordered']).dt.year
 mask = var == 2017
-
 initials = df['name'].apply(initialize)
 mask = mask & (initials == 'JP')
 # print(df[mask])
 
+### answer to candle 2
+combined_df = con.execute(query_combined).fetchdf()
+print(combined_df)
 
 con.close()
+
 
